619de0509e61482b4c2a3e6873732345f911643f
abishekchamlagai <52307155+abi123shek@users.noreply.github.com>
2025-02-02 15:38:06 +0545

    Update trivy_install.sh

--- Trivy/trivy_install.sh
+++ Trivy/trivy_install.sh
@@ -1,1 +1,10 @@
+#installation of trivy application on agent machine
 
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y wget
+wget https://github.com/aquasecurity/trivy/releases/download/v0.29.1/trivy_0.29.1_Linux-64bit.deb
+sudo dpkg -i trivy_0.29.1_Linux-64bit.deb
+trivy --version
+echo "downloaded Sucessfully"
